0ddc9c6972e209c96882681023e42209e696dab5
FurqanAhmed <furqanshaheer@gmail.com>
2022-09-05 22:15:13 +0500

    removed redundant import

--- lib/controllers/profile_controller.dart
+++ lib/controllers/profile_controller.dart
@@ -6,7 +6,6 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:task/controllers/auth_controller.dart';
-import 'package:task/root.dart';
 import 'package:task/screens/auth/login.dart';
 
 import '../models/user.dart';
